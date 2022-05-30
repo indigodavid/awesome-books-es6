921317c66fe1c785f2d2edb4c7fb3f3fe453c27d
David Vera <indigodavid@gmail.com>
2022-05-30 12:19:40 -0500

    Add toggleVisibility.js and link to index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 import Book from './modules/book.js';
 import getLi from './modules/getLi.js';
 import StorageBooks from './modules/storageBooks.js';
+import toggleVisbility from './modules/toggleVisibility.js';
---
+++ modules/toggleVisibility.js
@@ -0,0 +1,4 @@
+export default function toggleVisbility(activeEle) {
+  activeEle.classList.toggle('active');
+  activeEle.classList.remove('hide-class');
+}
