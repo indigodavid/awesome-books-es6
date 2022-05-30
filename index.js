20884f42cccf9525bfc01b8c0436680b35c7e0c3
David Vera <indigodavid@gmail.com>
2022-05-30 12:05:17 -0500

    Add index.js and modules/book.js and link first module

---
+++ index.js
@@ -0,0 +1,1 @@
+import Book from './modules/book.js';
---
+++ modules/book.js
@@ -0,0 +1,7 @@
+export default class Book {
+  constructor(title = 'New Book', author = 'John Doe', id) {
+    this.title = title;
+    this.author = author;
+    this.id = id;
+  }
+}
