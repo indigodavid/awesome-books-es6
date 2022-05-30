3f40bd233549aa6aec5efd8d1a928c1e7a0e6939
David Vera <indigodavid@gmail.com>
2022-05-30 12:24:06 -0500

    Add clearClasses.js and link to index.js

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ import Book from './modules/book.js';
 import getLi from './modules/getLi.js';
 import StorageBooks from './modules/storageBooks.js';
 import toggleVisbility from './modules/toggleVisibility.js';
+import clearClasses from './modules/clearClasses.js';
---
+++ modules/clearClasses.js
@@ -0,0 +1,7 @@
+export default function clearClasses(element1, element2) {
+  element1.classList.remove('active');
+  element1.classList.add('hide-class');
+
+  element2.classList.remove('active');
+  element2.classList.add('hide-class');
+}
